29cb5cdc6f72457b21393b68108894876c7cf0e1
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-11-23 13:31:01 +0200

    Pick randow winner

--- 1-Dev-lottery/Lotyery.sol
+++ 1-Dev-lottery/Lotyery.sol
@@ -10,12 +10,38 @@ contract Lottery{
         owner = msg.sender;
     }
 
+    modifier ownerOnly {
+        if(owner == msg.sender){
+            _;
+        }
+    }
+
     function deposit()public payable{
         require(msg.value >= 1 ether);
         players.push(msg.sender);
     }
 
-    
+    function generateRandom() public view returns (uint){
+        uint random =uint( keccak256(abi.encodePacked(now, block.difficulty, players.length)));
+        return random;
+    }
+
+    function pickWinner() ownerOnly() public{
+        uint genetated = generateRandom();
+        uint index = genetated % players.length;
+
+        address winner = players[index];
+
+        winner.transfer(address(this).balance);
+
+        players = new address payable [](0);
+    }
+
+
+
+
+
+
 
 }
 
