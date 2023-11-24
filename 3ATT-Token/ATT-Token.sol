e5ba7edc1a1a02b43f2d3c94a1cbb3440978113b
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-11-24 12:00:52 +0200

    Build AfricanTrade Token token using ERC-20 tokens

---
+++ 3ATT-Token/ATT-Token.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.0;
+
+// Import ERC20 from OpenZeppelin
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract ATToken is ERC20{
+     constructor () ERC20('AfricTrade Token','ATT') {
+        _mint(msg.sender, 200 * (10 ** 18));
+    }
+}
