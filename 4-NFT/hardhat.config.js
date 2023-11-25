8ede6f5d38e2c8f948457ffe9444789be00cae7a
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-11-25 09:45:28 +0200

    Configure the Sepolia network and the wallet

--- 4-NFT/hardhat.config.js
+++ 4-NFT/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+
+  networks:{
+    // Define the Sepolia network parameters here
+    sepolia: {
+      url: process.env.QUICKNODE_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
 };
