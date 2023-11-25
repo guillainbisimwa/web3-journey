604e172402418b7bfe232ce2d4aca4f9ad1c318e
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-11-25 12:48:52 +0200

    Fix issues

--- 4-NFT/scripts/deploy.js
+++ 4-NFT/scripts/deploy.js
@@ -1,22 +1,31 @@
+// Import the Hardhat package
+const hre = require("hardhat");
 
-const hre = require('hardhat');
+async function main() {
+  // Using `hre` - Hardhat Runtime Environment - we find and deploy
+  // a contract named `NFTee`
+  try {
 
+    // Get the contract owner
+    const contractOwner = await hre.ethers.getSigners();
+    console.log(`Deploying contract from: ${contractOwner[0].address}`);
 
-const main = async () => {
+    const nftContract = await hre.ethers.deployContract("NFTee");
 
-  const nftContact = await hre.ethers.deployContract("NFTee");
+    // We wait for the contract to finish deploying
+    await nftContract.waitForDeployment();
 
-  // Wait for contract to be deployed
-  await nftContact.waitForDeployment();
+    // We print the address of the deployed contract to our console
+    console.log("NFT Contract Address:", nftContract.target);
+  } catch (e) {
+    console.log('eroor: ', e);
+  }
+}
 
-  // Print address of our deplyed contract in the console
-  console.log('NFT Contract: ', nftContact.target)
-
-};
-
-main().then(()=> process.exit(0)).catch(
-  (error)=> {
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
     console.error(error);
     process.exit(1);
-  }
-);
+  });
