f04a693a13b08d98b180ca207516a6d4c4aa545f
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-12-20 13:38:18 +0200

    Rename folder

---
+++ 7-NFT-powered/frontend/constants.js
@@ -0,0 +1,7 @@
+export const CryptoDevsNFTAddress = "0xBE44C17BDDF930C62F2bCd62B84E4e2Dc0Bd591C";
+export const FakeNFTMarketplaceAddress = "REPLACE_WITH_YOUR_CONTRACT_ADDRESS";
+export const CryptoDevsDAOAddress = "REPLACE_WITH_YOUR_CONTRACT_ADDRESS";
+
+export const CryptoDevsNFTABI = []; // REPLACE THIS WITH THE NFT CONTRACT ABI
+export const FakeNFTMarketplaceABI = []; // REPLACE THIS WITH THE FAKE MARKETPLACE ABI
+export const CryptoDevsDAOABI = []; // REPLACE THIS WITH THE DAO ABI
